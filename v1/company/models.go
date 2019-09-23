d2055929d57fae76f2c4db6e68908fd0e5379754
Yeison <YeisonGutierrez07>
2019-09-22 23:58:58 -0500

    Agregar servicios para listar, crear, cambiar estado y eliminar empresas

--- v1/company/models.go
+++ v1/company/models.go
@@ -5,10 +5,35 @@ import "github.com/motorDoc-api/v1/global"
 // Company Modelo para los empresas
 type Company struct {
 	global.BaseModel
-	UserID int64 `json:"user_id" db:"user_id"`
+	UserID       int64   `json:"user_id" db:"user_id"`
+	BusinessName string  `json:"business_name" db:"business_name"`
+	Nit          string  `json:"nit" db:"nit"`
+	Logo         string  `json:"logo" db:"logo"`
+	Status       int     `json:"status" db:"status"`
+	Latitude     float64 `json:"latitude" db:"latitude" binding:"required"`
+	Longitude    float64 `json:"longitude" db:"longitude" binding:"required"`
 }
 
 type companyClients struct {
 	ClientID  int64 `json:"client_id" db:"client_id"`
 	CompanyID int64 `json:"company_id" db:"company_id"`
+}
+
+// CreateCompany Modelo para el servicio de crear empresas
+type CreateCompany struct {
+	Name         string `json:"name" binding:"required"`
+	LastName     string `json:"last_name" binding:"required"`
+	Address      string `json:"address" binding:"required"`
+	Email        string `json:"email" binding:"required"`
+	Credential   string `json:"credential" binding:"required"`
+	ProfilePic   string `json:"profile_pic" binding:"required"`
+	MobilePhone  int64  `json:"mobile_phone" binding:"required"`
+	BusinessName string `json:"business_name" binding:"required"`
+	ImageCompany string `json:"image_company" binding:"required"`
+	Nit          string `json:"nit" binding:"required"`
+}
+
+type changeStatus struct {
+	CompanyID int64 `json:"company_id" binding:"required"`
+	Status    int   `json:"status"`
 }
--- v1/users/models.go
+++ v1/users/models.go
@@ -2,34 +2,38 @@ package users
 
 import "github.com/motorDoc-api/v1/global"
 
+const (
+	ROLE_SUPERADMIN = "SUPERADMIN"
+	ROLE_COMPANY    = "COMPANY"
+	ROLE_MECHANIC   = "MECHANIC"
+	ROLE_CLIENT     = "CLIENT"
+)
+
 // User modelo de usuario
 type User struct {
 	global.BaseModel
-	Name        string  `json:"name" db:"name"`
-	LastName    string  `json:"last_name" db:"last_name"`
-	Password    string  `json:"-" db:"password" gorm:"not null"`
-	Email       string  `json:"email" db:"email" gorm:"UNIQUE_INDEX;NOT NULL"`
-	Address     string  `json:"address" db:"address"`
-	City        string  `json:"city" db:"city"`
-	MobilePhone string  `json:"mobilephone" db:"mobile_phone" gorm:"not null;"`
-	Credential  string  `json:"credential" db:"credential" gorm:"not null;UNIQUE_INDEX"`
-	ProfilePic  string  `json:"profile_pic" db:"profile_pic"`
-	Latitude    float64 `json:"latitude" db:"latitude"`
-	Longitude   float64 `json:"longitude" db:"longitude"`
+	Name        string `json:"name" db:"name"`
+	LastName    string `json:"last_name" db:"last_name"`
+	Password    string `json:"-" db:"password" gorm:"not null"`
+	Email       string `json:"email" db:"email" gorm:"UNIQUE_INDEX;NOT NULL"`
+	Address     string `json:"address" db:"address"`
+	City        string `json:"city" db:"city"`
+	Role        string `json:"role" db:"role"`
+	MobilePhone int64  `json:"mobilephone" db:"mobile_phone" gorm:"not null;"`
+	Credential  string `json:"credential" db:"credential" gorm:"not null;UNIQUE_INDEX"`
+	ProfilePic  string `json:"profile_pic" db:"profile_pic"`
 }
 
 // NewUser modelo de creación de una nueva cuenta
 type NewUser struct {
-	global.BaseModel
-	Name        string  `json:"name" db:"name" binding:"required"`
-	LastName    string  `json:"last_name" db:"last_name" binding:"required"`
-	Password    string  `json:"password" db:"password" gorm:"not null" binding:"required"`
-	Email       string  `json:"email" db:"email" gorm:"UNIQUE_INDEX;NOT NULL" binding:"required"`
-	Address     string  `json:"address" db:"address" binding:"required"`
-	City        string  `json:"city" db:"city" binding:"required"`
-	MobilePhone string  `json:"mobilephone" db:"mobile_phone" gorm:"not null;" binding:"required"`
-	Credential  string  `json:"credential" db:"credential" gorm:"not null;UNIQUE_INDEX" binding:"required"`
-	ProfilePic  string  `json:"profile_pic" db:"profile_pic" binding:"required"`
-	Latitude    float64 `json:"latitude" db:"latitude" binding:"required"`
-	Longitude   float64 `json:"longitude" db:"longitude" binding:"required"`
+	Name        string `json:"name" binding:"required"`
+	LastName    string `json:"last_name" binding:"required"`
+	Password    string `json:"password" binding:"required"`
+	Email       string `json:"email" binding:"required"`
+	Address     string `json:"address" binding:"required"`
+	City        string `json:"city" binding:"required"`
+	MobilePhone int64  `json:"mobilephone" binding:"required"`
+	Credential  string `json:"credential" binding:"required"`
+	ProfilePic  string `json:"profile_pic" binding:"required"`
+	Role        string `json:"role" binding:"required"`
 }
