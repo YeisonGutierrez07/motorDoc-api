3bc886c6087c5b7585be25b0bec6f5e9a105b6b8
Yeison <YeisonGutierrez07>
2019-10-05 23:01:18 -0500

    Agregar servicio para traer mecanicios, crearlos. ademas otro servicio para traer la info de la empresa logeada

--- v1/mechanic/controller.go
+++ v1/mechanic/controller.go
@@ -12,12 +12,47 @@ import (
 func Get(c *gin.Context) {
 	user := c.MustGet("user").(*users.User)
 
-	companies, err := GetMechanic(user)
+	mechanic, err := GetMechanic(user)
 	if err != nil {
 		response := global.ResponseServices(user, "400", err.Error())
 		c.JSON(http.StatusUnauthorized, response)
 		return
 	}
-	response := global.ResponseServices(companies, "200", "Exito")
+	response := global.ResponseServices(mechanic, "200", "Exito")
+	c.JSON(http.StatusOK, response)
+}
+
+// Create registrar de nuevos mecanicos
+func Create(c *gin.Context) {
+	user := c.MustGet("user").(*users.User)
+	mechanicData := CreateMechanic{}
+
+	err := c.ShouldBind(&mechanicData)
+	if err == nil {
+		errRegister := RegisterNewMechanic(user, mechanicData)
+		if errRegister != nil {
+			response := global.ResponseServices(mechanicData, "400", errRegister.Error())
+			c.JSON(400, response)
+			return
+		}
+		response := global.ResponseServices(mechanicData, "200", "Se he creado el usuario con exito")
+		c.JSON(http.StatusOK, response)
+		return
+	}
+	response := global.ResponseServices(mechanicData, "400", err.Error())
+	c.JSON(400, response)
+}
+
+// MisMechanics funcion para traerla  información de los mecanicos asiciados a el taller
+func MisMechanics(c *gin.Context) {
+	user := c.MustGet("user").(*users.User)
+
+	mechanic, err := GetMisMechanic(user)
+	if err != nil {
+		response := global.ResponseServices(user, "400", err.Error())
+		c.JSON(http.StatusUnauthorized, response)
+		return
+	}
+	response := global.ResponseServices(mechanic, "200", "Exito")
 	c.JSON(http.StatusOK, response)
 }
